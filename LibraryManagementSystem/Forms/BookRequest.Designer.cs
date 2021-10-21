ff055d03ad5c35c46aeccfcdf41163d7cf52fdcd
vatanak <koeurnsereyvatanak@gmail.com>
2021-10-21 20:24:30 +0700

    :memo: Designed Book Request Form

---
+++ LibraryManagementSystem/Forms/BookRequest.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace LibraryManagementSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBoxInput = new System.Windows.Forms.GroupBox();
+            this.tbBookName = new System.Windows.Forms.TextBox();
+            this.textBoxAuthor = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBoxInput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(58, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(687, 108);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Dangrek", 30F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(256, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Request Book";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::LibraryManagementSystem.Properties.Resources.book;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(184, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(66, 62);
+            this.panel1.TabIndex = 1;
+            // 
+            // groupBoxInput
+            // 
+            this.groupBoxInput.Controls.Add(this.btnSubmit);
+            this.groupBoxInput.Controls.Add(this.label3);
+            this.groupBoxInput.Controls.Add(this.label2);
+            this.groupBoxInput.Controls.Add(this.textBoxAuthor);
+            this.groupBoxInput.Controls.Add(this.tbBookName);
+            this.groupBoxInput.Location = new System.Drawing.Point(58, 131);
+            this.groupBoxInput.Name = "groupBoxInput";
+            this.groupBoxInput.Size = new System.Drawing.Size(687, 227);
+            this.groupBoxInput.TabIndex = 1;
+            this.groupBoxInput.TabStop = false;
+            // 
+            // tbBookName
+            // 
+            this.tbBookName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.tbBookName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.HistoryList;
+            this.tbBookName.Font = new System.Drawing.Font("Dangrek", 12F);
+            this.tbBookName.Location = new System.Drawing.Point(250, 19);
+            this.tbBookName.Name = "tbBookName";
+            this.tbBookName.Size = new System.Drawing.Size(264, 36);
+            this.tbBookName.TabIndex = 0;
+            // 
+            // textBoxAuthor
+            // 
+            this.textBoxAuthor.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBoxAuthor.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.HistoryList;
+            this.textBoxAuthor.Font = new System.Drawing.Font("Dangrek", 12F);
+            this.textBoxAuthor.Location = new System.Drawing.Point(250, 75);
+            this.textBoxAuthor.Name = "textBoxAuthor";
+            this.textBoxAuthor.Size = new System.Drawing.Size(264, 36);
+            this.textBoxAuthor.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Dangrek", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSubmit.Location = new System.Drawing.Point(262, 143);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(164, 62);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "SUBMIT";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Dangrek", 12F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(138, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 27);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Book Name:";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Dangrek", 12F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(138, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 27);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Author Name:";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxInput);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.Name = "Form1";
+            this.Text = "Request New Book";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBoxInput.ResumeLayout(false);
+            this.groupBoxInput.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBoxInput;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox textBoxAuthor;
+        private System.Windows.Forms.TextBox tbBookName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
--- LibraryManagementSystem/Forms/BookRequest.cs
+++ LibraryManagementSystem/Forms/BookRequest.cs
@@ -16,5 +16,15 @@ namespace LibraryManagementSystem
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
