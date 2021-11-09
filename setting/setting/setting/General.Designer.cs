e4f965c49a7712b576a49c1e800c58858c157fcb
Sky <79532409+Bormey-Sky@users.noreply.github.com>
2021-11-09 17:13:12 +0700

    Add files via upload
    :memo: add setting files

---
+++ setting/setting/setting/General.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace setting
+{
+    partial class General
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelPers = new System.Windows.Forms.Label();
+            this.labelNotif = new System.Windows.Forms.Label();
+            this.labelGeneral = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelAddr = new System.Windows.Forms.Label();
+            this.labelPn = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelPw = new System.Windows.Forms.Label();
+            this.labelDoB = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.labelPers);
+            this.panel1.Controls.Add(this.labelNotif);
+            this.panel1.Controls.Add(this.labelGeneral);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(346, 921);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelPers
+            // 
+            this.labelPers.AutoSize = true;
+            this.labelPers.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.labelPers.ForeColor = System.Drawing.Color.White;
+            this.labelPers.Location = new System.Drawing.Point(48, 308);
+            this.labelPers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPers.Name = "labelPers";
+            this.labelPers.Size = new System.Drawing.Size(215, 37);
+            this.labelPers.TabIndex = 3;
+            this.labelPers.Text = "Personalize";
+            this.labelPers.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelNotif
+            // 
+            this.labelNotif.AutoSize = true;
+            this.labelNotif.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.labelNotif.ForeColor = System.Drawing.Color.White;
+            this.labelNotif.Location = new System.Drawing.Point(40, 435);
+            this.labelNotif.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelNotif.Name = "labelNotif";
+            this.labelNotif.Size = new System.Drawing.Size(233, 37);
+            this.labelNotif.TabIndex = 2;
+            this.labelNotif.Text = "Notification";
+            // 
+            // labelGeneral
+            // 
+            this.labelGeneral.AutoSize = true;
+            this.labelGeneral.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.labelGeneral.ForeColor = System.Drawing.Color.White;
+            this.labelGeneral.Location = new System.Drawing.Point(84, 192);
+            this.labelGeneral.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelGeneral.Name = "labelGeneral";
+            this.labelGeneral.Size = new System.Drawing.Size(143, 37);
+            this.labelGeneral.TabIndex = 1;
+            this.labelGeneral.Text = "General";
+            this.labelGeneral.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(-12, 38);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 63);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Settings";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Consolas", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.ForeColor = System.Drawing.Color.White;
+            this.labelName.Location = new System.Drawing.Point(458, 125);
+            this.labelName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(90, 32);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Name:";
+            this.labelName.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Font = new System.Drawing.Font("Consolas", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEmail.ForeColor = System.Drawing.Color.White;
+            this.labelEmail.Location = new System.Drawing.Point(458, 351);
+            this.labelEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(105, 32);
+            this.labelEmail.TabIndex = 2;
+            this.labelEmail.Text = "Email:";
+            // 
+            // labelAddr
+            // 
+            this.labelAddr.AutoSize = true;
+            this.labelAddr.Font = new System.Drawing.Font("Consolas", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAddr.ForeColor = System.Drawing.Color.White;
+            this.labelAddr.Location = new System.Drawing.Point(458, 771);
+            this.labelAddr.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelAddr.Name = "labelAddr";
+            this.labelAddr.Size = new System.Drawing.Size(135, 32);
+            this.labelAddr.TabIndex = 7;
+            this.labelAddr.Text = "Address:";
+            // 
+            // labelPn
+            // 
+            this.labelPn.AutoSize = true;
+            this.labelPn.Font = new System.Drawing.Font("Consolas", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPn.ForeColor = System.Drawing.Color.White;
+            this.labelPn.Location = new System.Drawing.Point(458, 571);
+            this.labelPn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPn.Name = "labelPn";
+            this.labelPn.Size = new System.Drawing.Size(210, 32);
+            this.labelPn.TabIndex = 4;
+            this.labelPn.Text = "Phone Number:";
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Font = new System.Drawing.Font("Consolas", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelID.ForeColor = System.Drawing.Color.White;
+            this.labelID.Location = new System.Drawing.Point(458, 238);
+            this.labelID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(60, 32);
+            this.labelID.TabIndex = 6;
+            this.labelID.Text = "ID:";
+            // 
+            // labelPw
+            // 
+            this.labelPw.AutoSize = true;
+            this.labelPw.Font = new System.Drawing.Font("Consolas", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPw.ForeColor = System.Drawing.Color.White;
+            this.labelPw.Location = new System.Drawing.Point(458, 465);
+            this.labelPw.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPw.Name = "labelPw";
+            this.labelPw.Size = new System.Drawing.Size(150, 32);
+            this.labelPw.TabIndex = 3;
+            this.labelPw.Text = "Password:";
+            // 
+            // labelDoB
+            // 
+            this.labelDoB.AutoSize = true;
+            this.labelDoB.Font = new System.Drawing.Font("Consolas", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDoB.ForeColor = System.Drawing.Color.White;
+            this.labelDoB.Location = new System.Drawing.Point(458, 675);
+            this.labelDoB.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDoB.Name = "labelDoB";
+            this.labelDoB.Size = new System.Drawing.Size(225, 32);
+            this.labelDoB.TabIndex = 5;
+            this.labelDoB.Text = "Date of Birth:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(690, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(406, 31);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(690, 465);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(406, 31);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(690, 571);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(406, 31);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(690, 675);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(406, 31);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(690, 351);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(406, 31);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(690, 238);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(406, 31);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(690, 771);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(406, 31);
+            this.textBox8.TabIndex = 14;
+            // 
+            // General
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(99)))), ((int)(((byte)(102)))));
+            this.ClientSize = new System.Drawing.Size(1444, 919);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.labelAddr);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.labelDoB);
+            this.Controls.Add(this.labelPn);
+            this.Controls.Add(this.labelPw);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "General";
+            this.Text = "Settings";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelGeneral;
+        private System.Windows.Forms.Label labelPers;
+        private System.Windows.Forms.Label labelNotif;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelAddr;
+        private System.Windows.Forms.Label labelPn;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelPw;
+        private System.Windows.Forms.Label labelDoB;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
