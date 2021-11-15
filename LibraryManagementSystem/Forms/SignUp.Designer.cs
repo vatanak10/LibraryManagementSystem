682b8ee76b02aa1fb64dcfc2e5f74dcfe7e49cd1
Ftyma <featthimabou1@gmail.com>
2021-11-15 23:05:14 +0700

    Add & Fix Sign Up form

--- LibraryManagementSystem/Forms/SignUp.Designer.cs
+++ LibraryManagementSystem/Forms/SignUp.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace LibraryManagementSystem.Forms
+﻿namespace LibraryManagementSystem.Forms
 {
     partial class SignUp
     {
@@ -29,164 +28,164 @@ namespace LibraryManagementSystem.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.txtStuID = new System.Windows.Forms.TextBox();
-            this.txtEmail = new System.Windows.Forms.TextBox();
-            this.txtPW = new System.Windows.Forms.TextBox();
-            this.txtConfirmPW = new System.Windows.Forms.TextBox();
-            this.btnSubmit = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(693, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Library Membership Form";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(-24, 3);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(840, 106);
-            this.panel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.85714F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(100, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(640, 59);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Library Membership Form";
+            this.panel1.Size = new System.Drawing.Size(808, 90);
+            this.panel1.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(146, 189);
+            this.label2.Location = new System.Drawing.Point(150, 144);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(170, 32);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Full Name :";
+            this.label2.Size = new System.Drawing.Size(178, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Full Name : ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(146, 261);
+            this.label3.Location = new System.Drawing.Point(150, 209);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(175, 32);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Student ID :";
+            this.label3.Size = new System.Drawing.Size(183, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Student ID : ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(207, 333);
+            this.label4.Location = new System.Drawing.Point(216, 275);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(109, 32);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Email :";
+            this.label4.Size = new System.Drawing.Size(117, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Email : ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(152, 409);
+            this.label5.Location = new System.Drawing.Point(161, 347);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(164, 32);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Password :";
+            this.label5.Size = new System.Drawing.Size(172, 32);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Password : ";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(43, 481);
+            this.label6.Location = new System.Drawing.Point(47, 420);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(278, 32);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Confirm Password :";
+            this.label6.Size = new System.Drawing.Size(286, 32);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Confirm Password : ";
             // 
-            // txtName
+            // textBox1
             // 
-            this.txtName.Location = new System.Drawing.Point(390, 174);
-            this.txtName.Multiline = true;
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(360, 50);
-            this.txtName.TabIndex = 6;
+            this.textBox1.Location = new System.Drawing.Point(385, 125);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(352, 50);
+            this.textBox1.TabIndex = 7;
             // 
-            // txtStuID
+            // textBox2
             // 
-            this.txtStuID.Location = new System.Drawing.Point(390, 243);
-            this.txtStuID.Multiline = true;
-            this.txtStuID.Name = "txtStuID";
-            this.txtStuID.Size = new System.Drawing.Size(360, 50);
-            this.txtStuID.TabIndex = 7;
+            this.textBox2.Location = new System.Drawing.Point(385, 195);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(352, 52);
+            this.textBox2.TabIndex = 8;
             // 
-            // txtEmail
+            // textBox3
             // 
-            this.txtEmail.Location = new System.Drawing.Point(390, 315);
-            this.txtEmail.Multiline = true;
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(360, 50);
-            this.txtEmail.TabIndex = 8;
+            this.textBox3.Location = new System.Drawing.Point(385, 264);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(352, 52);
+            this.textBox3.TabIndex = 9;
             // 
-            // txtPW
+            // textBox4
             // 
-            this.txtPW.Location = new System.Drawing.Point(390, 391);
-            this.txtPW.Multiline = true;
-            this.txtPW.Name = "txtPW";
-            this.txtPW.Size = new System.Drawing.Size(360, 50);
-            this.txtPW.TabIndex = 9;
+            this.textBox4.Location = new System.Drawing.Point(385, 334);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(352, 52);
+            this.textBox4.TabIndex = 10;
             // 
-            // txtConfirmPW
+            // textBox5
             // 
-            this.txtConfirmPW.Location = new System.Drawing.Point(390, 463);
-            this.txtConfirmPW.Multiline = true;
-            this.txtConfirmPW.Name = "txtConfirmPW";
-            this.txtConfirmPW.Size = new System.Drawing.Size(360, 50);
-            this.txtConfirmPW.TabIndex = 10;
+            this.textBox5.Location = new System.Drawing.Point(385, 406);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(352, 52);
+            this.textBox5.TabIndex = 11;
             // 
-            // btnSubmit
+            // button1
             // 
-            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubmit.Location = new System.Drawing.Point(571, 561);
-            this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(179, 50);
-            this.btnSubmit.TabIndex = 11;
-            this.btnSubmit.Text = "Submit";
-            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(603, 502);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 60);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // btnExit
+            // button2
             // 
-            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(347, 561);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(179, 50);
-            this.btnExit.TabIndex = 12;
-            this.btnExit.Text = "Exit";
-            this.btnExit.UseVisualStyleBackColor = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(431, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 60);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // SignUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 648);
-            this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.btnSubmit);
-            this.Controls.Add(this.txtConfirmPW);
-            this.Controls.Add(this.txtPW);
-            this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.txtStuID);
-            this.Controls.Add(this.txtName);
+            this.ClientSize = new System.Drawing.Size(800, 602);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -195,7 +194,6 @@ namespace LibraryManagementSystem.Forms
             this.Controls.Add(this.panel1);
             this.Name = "SignUp";
             this.Text = "SignUp";
-            this.Load += new System.EventHandler(this.SignUp_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -205,19 +203,19 @@ namespace LibraryManagementSystem.Forms
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.TextBox txtStuID;
-        private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.TextBox txtPW;
-        private System.Windows.Forms.TextBox txtConfirmPW;
-        private System.Windows.Forms.Button btnSubmit;
-        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
